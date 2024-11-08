a7bba1a2a9c68ab6c3887e940b40048e04001cae
c.peters <c.peters@LT-NL-0051>
2024-11-08 11:20:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Bewaar_Vernietig_App;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes
+        .Add("https://graph.microsoft.com/User.Read");
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+await builder.Build().RunAsync();
